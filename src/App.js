6415a2ee251efe150f19a7298e36fe97862990dd
NicMortelliti <npmortelliti@gmail.com>
2022-06-23 11:24:22 -0700

    Grommet styling setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { Grommet } from 'grommet';
 
+const theme = {
+  global: {
+    colors: {
+      brand: '#228BE6',
+    },
+    font: {
+      family: 'Roboto',
+      size: '14px',
+      height: '20px',
+    },
+  },
+};
+
 const App = () => {
   return (
-    <div>
+    <Grommet theme={theme} full>
       <h1>Hello, world!</h1>
-    </div>
+    </Grommet>
   );
 }
 
