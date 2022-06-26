6cd89e0c4023736f063aa3e93bbaf10a5e7b1790
NicMortelliti <npmortelliti@gmail.com>
2022-06-25 17:08:58 -0700

    Add example form entry

--- src/components/EditForm.js
+++ src/components/EditForm.js
@@ -1,7 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Add, Close } from 'grommet-icons'
+import {
+  Box,
+  Button,
+  FormField,
+  Heading,
+  Layer,
+  Select,
+  TextArea,
+  TextInput,
+} from 'grommet'
 
 function EditForm() {
-  return <h3>EditForm</h3>
+  const [open, setOpen] = useState(false)
+  const [select, setSelect] = useState('')
+
+  const onOpen = () => setOpen(true)
+
+  const onClose = () => setOpen(undefined)
+
+  return (
+    <Box fill align='center' justify='center'>
+      {open && (
+        <Layer
+          position='right'
+          full='vertical'
+          modal
+          onClickOutside={onClose}
+          onEsc={onClose}
+        >
+          <Box
+            as='form'
+            fill='vertical'
+            overflow='auto'
+            width='medium'
+            pad='medium'
+            onSubmit={onclose}
+          >
+            <Box flex={false} direction="row" justify="between">
+              <Heading level={2} margin="none">
+                Add
+              </Heading>
+              <Button icon={<Close />} onClick={onClose} />
+            </Box>
+            <Box flex="grow" overflow="auto" pad={{ vertical: 'medium' }}>
+              <FormField label="First">
+                <TextInput/>
+              </FormField>
+              <FormField label="Second">
+                <Select
+                  options={[
+                    'one',
+                    'two',
+                    'three',
+                    'four',
+                    'five',
+                    'six',
+                    'seven',
+                    'eight',
+                  ]}
+                  value={select}
+                  onSearch={() => {}}
+                  onChange={({ option }) => setSelect(option)}
+                />
+              </FormField>
+              <FormField label="Third">
+                <TextArea />
+              </FormField>
+            </Box>
+            <Box flex={false} as="footer" align="start">
+              <Button type="submit" label="Submit" onClick={onClose} primary />
+            </Box>            
+
+          </Box>
+        </Layer>
+      )}
+    </Box>
+  )
 }
 
 export default EditForm
