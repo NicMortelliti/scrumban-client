8f57bf089478d6b30ecf74c6bf287a593fab7b50
NicMortelliti <npmortelliti@gmail.com>
2022-06-26 19:48:09 -0700

    Remove context approach for prop passing.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
-import { CurrentTaskProvider } from "./context/currentTask";
-
-// Import contexts
-import { EditOpenProvider } from "./context/editOpen";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <EditOpenProvider>
-    <CurrentTaskProvider>
-      <App />
-    </CurrentTaskProvider>
-  </EditOpenProvider>
-);
+root.render(<App />);
