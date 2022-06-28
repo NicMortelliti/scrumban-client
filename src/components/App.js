85cfd944def181bc1aec4cbcac96a99c73abc055
Nic Mortelliti <npmortelliti@gmail.com>
2022-06-27 18:23:32 -0700

    Update site name and remove unused code.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,27 @@
+import React, { useEffect, useState } from "react";
+
+const URL = "http://localhost:9292";
 
 const App = () => {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    fetch(`${URL}/`)
+      .then((r) => r.json())
+      .then((data) => setData(data));
+  }, []);
+
+  const DisplayTasks = () => {
+    console.log(data)
+    // data.map((eachTask) => {
+    //   return <li>{eachTask.description}</li>;
+    // });
+  };
   return (
     <div>
-      <h1>Hello, world!</h1>
+      <DisplayTasks />
     </div>
   );
-}
+};
 
 export default App;
