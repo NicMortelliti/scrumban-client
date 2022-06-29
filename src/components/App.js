e7b54b74a1ef38b1d2090ebd59abd2d5e2d2e485
NicMortelliti <npmortelliti@gmail.com>
2022-06-28 19:53:28 -0700

    Fetch users and projects.

--- src/components/App.js
+++ src/components/App.js
@@ -4,20 +4,45 @@ import TaskCard from "./TaskCard";
 const URL = "http://localhost:9292";
 
 const App = () => {
-  const [data, setData] = useState();
+  const [data, setData] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [projects, setProjects] = useState([]);
 
   // Fetch data from server
   useEffect(() => {
-    fetch(`${URL}/`)
-      .then((r) => r.json())
-      .then((data) => setData(data));
+    [
+      {
+        category: "/",
+        setter: setData,
+      },
+      {
+        category: "/users",
+        setter: setUsers,
+      },
+      {
+        category: "/projects",
+        setter: setProjects,
+      },
+    ].forEach((state) => {
+      fetch(`${URL}${state.category}`)
+        .then((r) => r.json())
+        .then((data) => state.setter(data));
+    });
   }, []);
 
   // Display tasks
   const DisplayItems = () => {
     return (
       data &&
-      data.map((eachTask) => <TaskCard key={eachTask.id} task={eachTask} data={data} setData={setData} />)
+      data.map((eachTask) => (
+        <TaskCard
+          key={eachTask.id}
+          task={eachTask}
+          data={data}
+          users={users}
+          setData={setData}
+        />
+      ))
     );
   };
 
