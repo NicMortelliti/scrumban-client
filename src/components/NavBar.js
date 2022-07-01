bad4c1833782c7ae5f325db6c2974af34fa3a454
NicMortelliti <npmortelliti@gmail.com>
2022-07-01 15:11:35 -0700

    Add comments to code

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,40 +3,51 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Dropdown from "react-bootstrap/Dropdown";
 
 function NavBar({ handleOpen, projects, currentProject, handleChange }) {
+  // Display Navbar.
+  // Display create task and project selection buttons
+  // if currentProject is not null
   const RenderNavBar = () => (
-    <Navbar sticky="top" bg="light" variant="light">
-      <Container>
-        <Navbar.Brand>Scrum Board</Navbar.Brand>
-        {currentProject && (
-          <>
-            <RenderAddButton />
-            <RenderDropDown />
-          </>
-        )}
-      </Container>
-    </Navbar>
+    <>
+      <Navbar sticky="top" bg="dark" variant="dark" border="dark">
+        <Container>
+          <Navbar.Brand>Scrum Board</Navbar.Brand>
+          {currentProject && (
+            <>
+              <RenderAddButton />
+              <RenderDropDown />
+            </>
+          )}
+        </Container>
+      </Navbar>
+      <br></br>
+    </>
   );
 
+  // Display button to create new task
   const RenderAddButton = () => (
-    <Button variant="outline-primary" onClick={(e) => handleOpen(e)}>
-      Add Task
+    <Button variant="primary" onClick={(e) => handleOpen(e)}>
+      Create Task
     </Button>
   );
 
+  // Display project selection drop down
   const RenderDropDown = () => (
-    <NavDropdown
-      title={currentProject.name}
+    <DropdownButton
       id="projectSelect"
+      title={currentProject.name}
+      variant="secondary"
+      align="end"
       onSelect={(e) => handleChange(e)}>
       {projects.map((project) => (
-        <NavDropdown.Item key={project.id} eventKey={project.id}>
+        <Dropdown.Item key={project.id} eventKey={project.id}>
           {project.name}
-        </NavDropdown.Item>
+        </Dropdown.Item>
       ))}
-    </NavDropdown>
+    </DropdownButton>
   );
 
   return <RenderNavBar />;
