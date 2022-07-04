58f5096e6f4384dd29bfd57344fd1cd086ba2ac9
Nic Mortelliti <npmortelliti@gmail.com>
2022-07-04 13:20:40 -0700

    Update ui of text entry field

--- src/components/TaskEditText.js
+++ src/components/TaskEditText.js
@@ -2,19 +2,21 @@ import React from "react";
 
 import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
 
 function TaskEditText({ label, name, value, handleChange }) {
   return (
     <>
-      <FloatingLabel label={`${label}:`}>
+      <InputGroup>
+        <InputGroup.Text id="btnGroupAddon">{label}</InputGroup.Text>
         <Form.Control
           type="text"
           name={name}
-          className="my-3"
+          // className="my-3"
           value={value}
           onChange={(e) => handleChange(e)}
         />
-      </FloatingLabel>
+      </InputGroup>
     </>
   );
 }
