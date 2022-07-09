c2391e459ee16d570e9e64b25e6e34191fd2fbda
Nic Mortelliti <npmortelliti@gmail.com>
2022-07-09 08:45:05 -0700

    More nord styling

--- src/components/ProjectSelectModal.js
+++ src/components/ProjectSelectModal.js
@@ -6,23 +6,29 @@ import Modal from "react-bootstrap/Modal";
 function ProjectSelectModal({ projects, handleChange }) {
   return (
     <Modal show size="lg" centered className="text-center">
-      <Modal.Header closeButton>
+      <Modal.Header closeButton className="bg-custom-dark nord6">
         <Modal.Title>Welcome to Scrum!</Modal.Title>
       </Modal.Header>
-      <br></br>
-      <Dropdown onSelect={(e) => handleChange(e)}>
-        <Dropdown.Toggle variant="primary" id="dropdown-basic">
-          Select a project
-        </Dropdown.Toggle>
-        <Dropdown.Menu>
-          {projects.map((project) => (
-            <Dropdown.Item key={project.id} eventKey={project.id}>
-              {project.name}
-            </Dropdown.Item>
-          ))}
-        </Dropdown.Menu>
-      </Dropdown>
-      <br></br>
+
+      <div className="bg-custom-dark">
+        <br />
+        <Dropdown onSelect={(e) => handleChange(e)}>
+          <Dropdown.Toggle
+            variant="primary"
+            id="dropdown-basic"
+            className="custom-primary-btn">
+            Select a project
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            {projects.map((project) => (
+              <Dropdown.Item key={project.id} eventKey={project.id}>
+                {project.name}
+              </Dropdown.Item>
+            ))}
+          </Dropdown.Menu>
+        </Dropdown>
+        <br />
+      </div>
     </Modal>
   );
 }
